3832c10970270cf728477a10e86524f6b5203a8e
Amandeep Verma <amandeepverma145@gmail.com>
2022-09-25 20:54:10 +0530

    second

--- app.py
+++ app.py
@@ -1,17 +1,56 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import streamlit as st
 import torch
+# import yake
+from rake_nltk import Rake
+rake_nltk_var = Rake()
+
 
 st.set_page_config(
-    page_title="Multipage App"
+    page_title="Khayal App"
 )
 st.sidebar.success("Select a page")
+#
+# page_bg_img = """
+# <style>
+# [data-testid="stAppViewContainer"] {
+# background-image url("https://images.unsplash.com/photo-1490633874781-1c63cc424610?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80");
+# background-size: cover;
+# }
+# <style>
+# """
+# st.markdown(page_bg_img, unsafe_allow_html= True)
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://images.unsplash.com/photo-1452423668729-43a98052d3ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         [data-testid="stHeader"] {{
+         background-color: rgba(0,0,0,0);
+         }}
+         [data-testid="stSidebar"] {{
+         background-image: url("https://images.unsplash.com/photo-1487528742387-d53d4f12488d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1922&q=80");
+         background-size: cover;
+         }}
+         </style>
+         
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url()
 
 
 
 
-base="dark"
-primaryColor="purple"
+
+#
+# base="dark"
+# primaryColor="purple"
 st.title("Poem Generator")
 st.markdown("This is a poem generator app. you can use it to create poems by providing text to it.")
 @st.cache(allow_output_mutation=True)
@@ -31,7 +70,7 @@ tokenizer,model = get_model()
 #prompt = input("Enter the start of poem here:")
 #button = st.button("Create")
 
-prompt = st.text_area("Enter Text:")
+prompt = st.text_input("Enter Text:",placeholder="Enter The text here")
 click = st.button("Create")
 if click and prompt:
     generated = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0)
@@ -49,10 +88,26 @@ if click and prompt:
         poemm = tokenizer.decode(sample_output, skip_special_tokens=True)
     # print("{}: {}\n\n".format(i,
 
-    st.text_area("Generated Poem:", poemm)
-    st.snow()
+
+    st.text_area("Generated Poem:", poemm,height=250)
+ #   st.snow()
     st.success("Poem Generated")
 
 
 
+# kw_extractor = yake.KeywordExtractor()
+# text = poemm
+# language = "en"
+# max_ngram_size = 3
+# deduplication_threshold = 0.9
+# numOfKeywords = 5
+# custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
+# keywords = custom_kw_extractor.extract_keywords(text)
+# for kw in keywords:
+#     print(kw)
 
+# from rake_nltk import Rake
+# rake_nltk_var = Rake()
+# rake_nltk_var.extract_keywords_from_text(poemm)
+# kw = rake_nltk_var.get_ranked_phrases()
+# st.t
---
+++ pages/Summarizer.py
@@ -0,0 +1,106 @@
+# This is a sample Python script.
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 11:11:45 2022
+
+@author: Amandeep Verma
+"""
+
+# Importing Libararies
+import streamlit as st
+import nltk
+from string import punctuation
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+
+def add_bg_from_url():
+  st.markdown(
+    f"""
+         <style>
+         .stApp {{
+             background-image: url("https://images.unsplash.com/photo-1505682499293-233fb141754c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         [data-testid="stHeader"] {{
+         background-color: rgba(0,0,0,0);
+         }}
+         [data-testid="stSidebar"] {{
+         background-image: url("https://images.unsplash.com/photo-1487528742387-d53d4f12488d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1922&q=80");
+         background-size: cover;
+         }}
+         </style>
+
+         """,
+    unsafe_allow_html=True
+  )
+
+
+add_bg_from_url()
+
+
+
+
+st.title("Text Summarization")
+st.header("Summary Generator")
+
+text = st.text_area("Enter your text here.",height=200)
+re = len(text.split())
+st.markdown("Number of words in text:")
+st.write(re)
+
+if st.button("Generate Summary"):
+    # Tokenising the text
+    nltk.download("stopwords")
+    stop_words = stopwords.words('english')
+    words = word_tokenize(text)
+
+    punctuation = punctuation + '\n'
+
+    # Creating the Frequency table
+    textfreq = {}
+    for word in words:
+        if word.lower() not in stop_words:
+            if word.lower() not in punctuation:
+                if word not in textfreq.keys():
+                    textfreq[word] = 1
+                else:
+                    textfreq[word] += 1
+
+
+    # To find the weighted frequency
+    maximum_frequncy = max(textfreq.values())
+
+    for word in textfreq.keys():
+        textfreq[word] = (textfreq[word] / maximum_frequncy)
+
+    # Tokenizing the text into sentences
+    sentences = sent_tokenize(text)
+
+    # Calculating the score of sentences
+    sent_score = {}
+    for sent in sentences:
+        sentence = sent.split(" ")
+        for word in sentence:
+            if word.lower() in textfreq.keys():
+                if sent not in sent_score.keys():
+                    sent_score[sent] = textfreq[word.lower()]
+                else:
+                    sent_score[sent] += textfreq[word.lower()]
+
+
+    # creation of summary
+    from heapq import nlargest
+
+    sentlength = int(len(sentences) * 0.3)
+
+    summary = nlargest(sentlength, sent_score, key=sent_score.get)
+    final_summary = [word for word in summary]
+
+    summary = ' '.join(final_summary)
+
+    st.write(summary)
+    res = len(summary.split())
+    st.markdown("Number of words in Summary:")
+    st.write(res)
