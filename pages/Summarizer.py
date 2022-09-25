ea4e1595c520377af5c08f3e383310dbede08446
Amandeep Verma <amandeepverma145@gmail.com>
2022-09-25 22:24:30 +0530

    f

--- pages/Summarizer.py
+++ pages/Summarizer.py
@@ -13,7 +13,7 @@ from string import punctuation
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
-
+nltk.download('punkt')
 def add_bg_from_url():
   st.markdown(
     f"""
